77097810da76c378a247d097ecc4179c4412e40e
Thakur-Aditya <87617889+Thakur-Aditya@users.noreply.github.com>
2024-12-26 03:47:18 +0530

    finishing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "./hoc";
+import { slideIn } from "../utils/motion";
+
+//serviceId - service_tqbrt1e
+//template id - template_s2i032e
+//public key - FgidWmRp1qdz1GgZV
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
 
-export default Contact
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_tqbrt1e",
+        "template_6hxnl4b",
+        {
+          from_name: form.name,
+          to_name: "Aditya Singh",
+          from_email: form.email,
+          to_email: "sengaraditya01@gmail.com",
+          message: form.message + " " + "from" + " " + form.email,
+        },
+        "FgidWmRp1qdz1GgZV"
+      )
+      .then(
+        () => {
+          alert("Thank You. I will get back to you as soon as possible.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+
+          setLoading(false);
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert(
+            "Something went wrong. Please try again later. or you contact mail me directly at sengaraditya01@gmail.com."
+          );
+        }
+      );
+  };
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "between", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form
+          action=""
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              className="bg-tertiary py-4 px-6 text-white rounded-lg outline-none font-medium placeholder:text-secondary"
+              placeholder="What's your name?"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              className="bg-tertiary py-4 px-6 text-white rounded-lg outline-none font-medium placeholder:text-secondary"
+              placeholder="What's your Email"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              className="bg-tertiary py-4 px-6 text-white rounded-lg outline-none font-medium placeholder:text-secondary"
+              placeholder="Say Something..."
+            />
+          </label>
+          <button
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            type="submit"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        className="xl:h-auto xl-flex md:h-[550px] h-[350px] flex-1"
+        variants={slideIn("right", "between", 0.2, 1)}
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
   return (
-    <div>Ball</div>
-  )
+    <Float speed={3} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[1,1, 2]} />
+      <mesh castShadow receiveShadow scale={4.5} rotation={[0, Math.PI/2, 0]}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="white"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          // debug
+          map={decal}
+          position={[0, 0, 0.9]}
+          flatShading
+          rotation={[0, 0, 0]}
+          // rotation={[Math.PI / 2, 0, 0]}
+          scale={[1.15,1.15,1.15]}
+        />
+      </mesh>
+    </Float>
+  );
+};
+
+function BallCanvas({ icon }) {
+  return (
+    <Canvas
+      frameloop="always"
+      camera={{ position: [20, 3, 5], fov: 24 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 }
 
-export default Ball
+export default BallCanvas;
